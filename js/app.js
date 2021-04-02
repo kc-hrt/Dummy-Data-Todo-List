fd638e52d30511ac32346b730a1893cd0a1a4749
kc-hrt <kaseyh@pm.me>
2021-04-01 20:17:55 -0500

    more changes

--- js/app.js
+++ js/app.js
@@ -17,8 +17,9 @@ let arrayOfTodos = [
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
   .then( (response) => response.json())
-  // .then( (json) => arrayOfTodos = json)
-  .then((json) => console.log(json));
+  .then( (json) => arrayOfTodos = json)
+  // .then((json) => carrayOfTo(json));
+  console.log('arrayOfTodos:',arrayOfTodos)
 }
 
 const logTodos = () => {
@@ -26,14 +27,33 @@ const logTodos = () => {
 }
 
 const populateTodos = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-  // create an element <li>
-  // get parent element <ol>
-  // 
+  // fetch('https://jsonplaceholder.typicode.com/todos')
+  // .then((response) => response.json())
+  // .then((json) => console.log(json));
+
+  //get ol tag to insert list items
+  const ol = document.getElementById('todo-list');
+
+  for (let index = 0; index < arrayOfTodos.length; index++) {
+    // create a new div element
+    const li = document.createElement("li");
+    // get title from object
+    const title = arrayOfTodos[index].title
+    // put title in the li element
+    const listItemText = document.createTextNode(title);
+    // add textNode to list element
+    li.appendChild(listItemText)
+    // add list item to ol element in DOM
+    ol.appendChild(li)
+    // document.body.insertBefore(li, ol);
+
+    // const element = arrayOfTodos[index];
+    // console.log('element:', element)
+
+  }
 }
 
 
 // console.log(arrayOfTodos1[0].userId) // => 14
 // console.log(arrayOfTodos1[1].userId) // => 20
+
